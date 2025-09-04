0a33e348a18c20add146a179d6bbd44bc20e5d6a
Hyeseon Lee <hyeseon1379@gmail.com>
2025-09-04 23:09:05 +0900

    refactor: 카테고리명 변경

--- src/main/java/ONDA/global/category/CategoryInitializer.java
+++ src/main/java/ONDA/global/category/CategoryInitializer.java
@@ -12,21 +12,12 @@ public class CategoryInitializer {
 
     @PostConstruct
     public void initCategories() {
-        save("외국어 · 번역 · 통역");
-        save("프로그래밍 · IT · 데이터");
-        save("디자인 · 크리에이티브");
-        save("영상 · 사진 · 콘텐츠 제작");
-        save("음악 · 보컬 · 악기");
-        save("미술 · 공예 · 수공예");
-        save("뷰티 · 패션 · 스타일링");
-        save("스포츠 · 피트니스 · 건강");
-        save("라이프 · 자기계발");
-        save("취업 · 진로 · 입시");
-        save("비즈니스 · 창업 · 마케팅");
-        save("경제 · 재테크 · 투자");
-        save("글쓰기 · 출판 · 스피치");
-        save("여행 · 문화 · 생활 취미");
-        save("심리 · 상담 · 힐링");
+        save("창의 • 예술");
+        save("라이프스타일");
+        save("스포츠 • 웰빙");
+        save("언어 • 학습");
+        save("IT • 디지털");
+        save("자기계발");
     }
 
     private void save(String name) {
